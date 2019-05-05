959656e48d01b16aa2f460d63e7ca42214e35504
Julian Hautzmayer <julian.hautzmayer@gmail.com>
2019-05-05 09:02:31 +0200

    Add files via upload

---
+++ Sektion_03_Abfragen/Code/03_player_score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Examples.sektion_03
+{
+    class _03_zahlen_sortieren
+    {
+        public static void Run()
+        {
+            int playerScore = 0;
+
+            Console.Write("Player score: ");
+            playerScore = Convert.ToInt32(Console.ReadLine());
+
+            if(playerScore > 20)
+            {
+                Console.WriteLine("Player has won the game!");
+            } 
+            else
+            {
+                Console.WriteLine("Nothing happened yet");
+            }
+        }
+    }
+}
